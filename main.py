c511c65982a10832ad53d6ff49daf68f756086b5
Gergana Vandova <faile@faile-laptop.(none)>
2010-04-01 23:51:13 +0200

    Now doing all the thresholds  and writing an excel file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 import sys
 
-# test comment
+# sudo easy_install xlwt
+
+import xlwt
+from tempfile import TemporaryFile
+from xlwt import Workbook
 
 thr1 = 0.066
 thr2 = -0.052
@@ -32,12 +36,9 @@ class NMR:
         for line in lines:
             d = line.split('\t')
             if len(d) == 11:
-#                print len(d), d[0], d[9]
                 num = int(d[0].strip())
                 val = int(d[9].strip())
                 self.add(num, is_disordered1(val))
-
-#        print self.data
 
     def parse2(self, filename):
         f = open(filename, "r")
@@ -48,7 +49,6 @@ class NMR:
             if len(d) == 2:
                 num = int(d[0]) + mismatch_fix
                 val = float(d[1])
-#               print num, val, is_disordered2(val)
                 self.add(num, is_disordered2(val))
 
 
@@ -79,17 +79,6 @@ class NMR:
 def usage():
     print "Usage: ./main.py <file1> <file2> thr1 mismatch_fix"
 
-if len(sys.argv) <> 5:
-    usage()
-
-thr3 = int(sys.argv[3])
-mismatch_fix = int(sys.argv[4])
-
-nmr1 = NMR()
-nmr1.parse1(sys.argv[1])
-
-nmr2 = NMR()
-nmr2.parse2(sys.argv[2])
 
 def compare(nmr1, nmr2):
     tp = 0
@@ -109,15 +98,111 @@ def compare(nmr1, nmr2):
                 tn += 1
             else:
                 print "SHIT"
-        i += 1
+        i += 1    
 
+    data_row = sheet1.row(thr3+1)
+    data_row.write(0, thr3)
+    
+    len_of_dis = tp+fn 
+    print "Length of Disorder: ", len_of_dis 
+    data_row.write(1, len_of_dis)
+    
+    hits = tp+tn
+    print "Hits: ", hits
+    data_row.write(2, hits)
+    
     print "True Positive: ", tp
+    data_row.write(3, tp)
     print "True Negative: ", tn
+    data_row.write(4, tn)
     print "False Positive: ", fp
+    data_row.write(5, fp)
     print "Flase Negative: ", fn
-    print "Accuracy: ", float(tp)/float(tp+fp)
+    data_row.write(6, fn)
+    
+    if tp+fp<>0:
+         accuracy = float(tp)/float(tp+fp)
+    else:
+        accuracy = "N/A"
+    print "Accuracy: ", accuracy
+    data_row.write(7, accuracy)
+           
+    if tp+fn<>0:
+        coverage = float(tp)/float(tp+fn)
+    else:
+        coverage = "N/A"
+    print "Coverage = TP rate: ", coverage
+    data_row.write(8, coverage)
+    
+    if fp+tn<>0:    
+        fp_rate = float(fp)/float(fp+tn)
+    else:
+        fp_rate = "N/A"
+    print "FP Rate: ", fp_rate
+    data_row.write(9, fp_rate)
+    
+   
+    if tn+fn<>0: 
+        accuracy_ordered = float(tn)/float(tn+fn)
+    else:
+        accuracy_ordered = "N/A"
+    print "Accuracy Ordered: ", accuracy_ordered
+    data_row.write(10, accuracy_ordered)
+        
+    if tp+fp<>0:
+        coverage_ordered = float(tn)/float(tn+fp)
+    else:
+        coverage_ordered = "N/A"
+    print "Coverage Ordered: ", coverage_ordered
+    data_row.write(11, coverage_ordered)
+    
+    
+if len(sys.argv) <> 5:
+    usage()
 
-compare(nmr1, nmr2)
+#thr3 = int(sys.argv[3])
+mismatch_fix = int(sys.argv[3])
+
+# some hacky code to get the protein number from the file name
+f = sys.argv[1]
+ar = f.split("/")
+protein_num = ar[len(ar)-1].split(".")[0]
+
+book = Workbook()
+sheet1 = book.add_sheet(protein_num)
+header_row = sheet1.row(0)
+header_row.write(0, "\\")
+header_row.write(1, "Length of Disorder")
+header_row.write(2, "Hits")
+header_row.write(3, "True Positives")
+header_row.write(4, "True Negative")
+header_row.write(5, "False Posotives")
+header_row.write(6, "False Negatives")
+header_row.write(7, "Accuracy")
+header_row.write(8, "Coverage/TP Rate")
+header_row.write(9, "FP Rate")
+header_row.write(10, "Accuracy Ordered")
+header_row.write(11, "Coverage Ordered")
+
+
+
+nmr2 = NMR()
+nmr2.parse2(sys.argv[2])
+
+
+for th in range(10):
+    thr3 = th
+    print "################################# ", thr3 
+
+    nmr1 = NMR()
+    nmr1.parse1(sys.argv[1])
+    
+    compare(nmr1, nmr2)
+
+
+
+book.save(protein_num + ".xls")
+book.save(TemporaryFile())
 
 
 
